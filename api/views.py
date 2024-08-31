b9bf229d3170ad396047ad55072e578102b9188e
Shubham Shirodkar <shubhamshirodkar90@gmail.com>
2024-08-31 23:17:11 +0530

    First Commit

---
+++ api/views.py
@@ -0,0 +1,36 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import Client, Project
+from .serializers import ClientSerializer, ProjectSerializer
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
+
+class ClientListCreateView(generics.ListCreateAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+class ClientRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    permission_classes = [IsAuthenticated]
+
+class ProjectCreateView(generics.CreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        client_id = self.kwargs.get('id')
+        client = Client.objects.get(pk=client_id)
+        serializer.save(client=client, created_by=self.request.user)
+
+class UserProjectsView(generics.ListAPIView):
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Project.objects.filter(users=self.request.user)
